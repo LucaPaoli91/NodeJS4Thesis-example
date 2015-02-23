92545224389c55e545e29ed3a1213c0ffb352560
unknown <luca.paoli2@gmail.com>
2015-02-23 10:25:12 +0100

    Profile image visualization problem solved

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var port     = process.env.PORT || 3000;
 var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
+var path     = require('path');
 
 var morgan       = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -26,7 +27,9 @@ require('./config/passport')(passport); // pass passport for configuration
 app.use(morgan('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
 app.use(bodyParser()); // get information from html forms
-app.use(multer({dest : './public/uploads/'})); //handle multipart upload request
+app.use(multer({dest : './public/uploads/'})); // handle multipart upload request
+
+app.use(express.static(path.join(__dirname, 'public'))); // handle public request to the "public" folder 
 
 app.set('view engine', 'ejs'); // set up ejs for templating
 
